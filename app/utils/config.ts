60e7d0844f795710625b74082804d23afc0469a9
Mitch Chanza <mitch.chanza@gmail.com>
2023-01-21 21:29:53 +0200

    chore: add  utils module

---
+++ app/utils/config.ts
@@ -0,0 +1,17 @@
+/**
+ * Manages all configurations in the application. 
+ * @author Mitch Chanza <mitch.chanza@gmail.com>
+ * @usage import this configuration in any file and use object destructuring to access respective configs
+ */
+
+export const environ = {
+    pms: {
+        /**pm2 configurations goes here */
+    },
+    prism: {
+        /** All prisma configurations goes here */
+    },
+    server: {
+        /** add any server configurations here */
+    }
+}
