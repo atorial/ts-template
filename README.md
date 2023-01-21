2f86ea8528de4f1bc44737c6d564354cc8c63d74
Mitch Chanza <mitch.chanza@gmail.com>
2023-01-21 21:23:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Atoria Typescript Template Library
+This is a template library that provides a simple interface for starting off a typical application.
---
+++ app/index.ts
@@ -0,0 +1,5 @@
+/**Gets current project version */
+const welcome = ()=> {
+    return "Welcome to the template " 
+}
+console.log(welcome()); // calls the function to update the version
